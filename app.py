34f3c3b03aa878d15629d7f136824b4af29a395d
Scott Sheppard <jssheppard83@gmail.com>
2018-10-19 04:03:09 -0500

    initial commit, before video 7 deletes the User class module and more

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, redirect, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flask-movie:flask-movie@localhost:8889/flask-movie'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+
+@app.route('/')
+def index():
+    myUser = User.query.all()
+    oneItem = User.query.filter_by(username="test2").first()
+    return render_template('add_user.html', myUser=myUser, oneItem=oneItem)
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    user = User.query.filter_by(username=username).first()
+    return render_template('profile.html', user=user)
+
+
+@app.route('/post_user', methods=['POST', 'GET'])
+def post_user():
+    user = User(request.form['username'], request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('index'))
+    
+
+if __name__ == "__main__":
+    app.run()
